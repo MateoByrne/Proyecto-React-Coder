b0771f9ed71c706ee65c198b8074652a4358e05a
Mateo Byrne <byrnemateo1@gmail.com>
2023-11-02 20:11:24 -0300

    Entrega 1 React

---
+++ vite-project/src/components/CartWidgetComponent/CartWidgetComponent.jsx
@@ -0,0 +1,12 @@
+
+
+const CartWidgetComponent = () => {
+    return (
+        <div>
+            <span>icono</span>
+            <span>10</span>
+        </div>
+    )
+}
+
+export default CartWidgetComponent;
