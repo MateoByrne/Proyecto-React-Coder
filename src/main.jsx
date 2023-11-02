68dcb5b1f5e1f942c6cf69f6d460aef61c2cd8f0
Mateo Byrne <byrnemateo1@gmail.com>
2023-11-02 20:36:27 -0300

    Entrega 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import NavBarComponent from '../src/components/NavBarComponent/NavBarComponent'
+import NavBarComponent from './components/NavBarComponent/NavBarComponent'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
